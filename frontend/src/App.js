cbd9f5bf1fea06053162513adec1aa19c919fa73
Cheol Jang <gcjjyy@gmail.com>
2025-12-01 15:07:16 +0900

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1086,13 +1086,7 @@ function App() {
           value={command}
           onChange={(event) => setCommand(event.target.value)}
           onKeyUp={(event) => onKeyUp(event.key)}
-          autoComplete="new-password"
-          autoCorrect="off"
-          autoCapitalize="off"
-          spellCheck="false"
-          data-form-type="other"
-          data-lpignore="true"
-          data-1p-ignore="true"
+          autoComplete="off"
         />
       </div>
       <div className="text-center mt-3">
